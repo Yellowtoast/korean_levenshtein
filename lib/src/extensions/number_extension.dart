6c8fd9f1a2ce758f02d84fbdc7b064b9e3d50ba0
jihye <kjh9519@naver.com>
2024-03-17 20:47:51 +0900

    Fix: fixed numberToKorean function in NumberExtention

--- lib/src/extensions/number_extension.dart
+++ lib/src/extensions/number_extension.dart
@@ -1,49 +1,42 @@
-List<String> _numberUnits = [
-  '',
-  '십',
-  '백',
-  '천',
-  '만',
-  '십만',
-  '백만',
-  '천만',
-  '억',
-  '십억',
-  '백억',
-  '천억',
-  '조',
-  '십조',
-  '백조',
-  '천조',
-  '경',
-  '십경',
-  '백경',
-  '천경'
-];
-List<String> _numberDigits = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
+import 'dart:math';
 
-extension NumberExtension on int {
+const _numberUnits = ["", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"];
+const _tenUnits = ["", "십", "백", "천"];
+const _thousandUnits = ["", "만", "억", "조", "경", "해"];
+
+extension NumberExtention on int {
   String numbersToKorean() {
-    if (this == 0) {
-      return '영';
-    }
-
-    List<int> numList = [];
-    int number = this;
-    while (number > 0) {
-      numList.add(number % 10);
-      number ~/= 10;
-    }
-
-    String result = '';
-    for (int i = numList.length - 1; i >= 0; i--) {
-      int digit = numList[i];
-      if (digit != 0) {
-        result += _numberDigits[digit];
-        result += _numberUnits[i];
+    List<String> _chunkAtEnd(String value, [int n = 1]) {
+      List<String> result = [];
+      for (int end = value.length; end > 0; end -= n) {
+        result.add(value.substring(max(0, end - n), end));
       }
+      return result;
     }
 
-    return result;
+    return _chunkAtEnd(toString(), 4).fold<String>("", (acc, item) {
+      if (int.parse(item) == 0) {
+        return acc;
+      }
+
+      String numberUnit = "";
+
+      String zeroNum = item.padLeft(4, "0");
+
+      for (int i = 0; i < 4; i++) {
+        int number = int.parse(zeroNum[i]);
+
+        if (number != 0) {
+          String unit = _tenUnits[3 - i];
+
+          numberUnit +=
+              "${unit.isNotEmpty && number == 1 ? "" : _numberUnits[number]}$unit";
+        }
+      }
+
+      String thousandUnit = _thousandUnits.removeAt(0);
+
+      return "${numberUnit + thousandUnit}$acc";
+    }).trim();
   }
 }
