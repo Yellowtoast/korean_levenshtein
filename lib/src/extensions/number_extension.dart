bcba108714b35bdce97373e3016fbc8b044d5f95
jihye <kjh9519@naver.com>
2024-03-24 15:02:10 +0900

    Docs: added comments to NumberExtension

--- lib/src/extensions/number_extension.dart
+++ lib/src/extensions/number_extension.dart
@@ -1,4 +1,7 @@
 extension NumberExtension on int {
+  /// Converts the integer number to Korean representation.
+  ///
+  /// Returns the Korean representation of the integer number.
   String numbersToKorean() {
     int number = this;
     List<String> koreanNumber = [
@@ -28,7 +31,7 @@ extension NumberExtension on int {
         int index = entry.key;
         int valueToNumber = int.parse(entry.value);
         if (valueToNumber == 0) return acc;
-        // 단위가 십 이상인 '일'글자는 출력하지 않는다. ex) 일십 -> 십
+        // Do not output '일' character for units above ten. ex) 일십 -> 십
         String numberToKorean = index < length && valueToNumber == 1
             ? ''
             : koreanNumber[valueToNumber];
@@ -42,54 +45,3 @@ extension NumberExtension on int {
     return answer.trim();
   }
 }
-
-// extension NumberExtension on int {
-//   static const List<String> _numberUnits = [
-//     "",
-//     "일",
-//     "이",
-//     "삼",
-//     "사",
-//     "오",
-//     "육",
-//     "칠",
-//     "팔",
-//     "구"
-//   ];
-//   static const List<String> _tenUnits = ["", "십", "백", "천"];
-//   static const List<String> _thousandUnits = ["", "만", "억", "조", "경", "해"];
-
-//   String numbersToKorean() {
-//     int number = this;
-//     if (number == 0) return '영';
-
-//     List<String> digits = ["", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"];
-//     List<String> units = ['', '십', '백', '천'];
-//     List<String> largeUnits = ['', '만', '억', '조', '경', '해'];
-
-//     String result = '';
-//     int unitIndex = 0;
-
-//     while (number > 0) {
-//       int digit = number % 10;
-//       number ~/= 10;
-
-//       if (digit > 0) {
-//         if (unitIndex % 4 == 0 && result.isNotEmpty) {
-//           result = largeUnits[unitIndex ~/ 4] + result;
-//         }
-//         result = units[unitIndex % 4] + result;
-//         if (digit != 1 || unitIndex % 4 != 1) {
-//           result = digits[digit] + result;
-//         }
-//       }
-//       unitIndex++;
-//     }
-
-//     return result;
-//   }
-// }
-
-// void main() {
-//   print(123456789.numbersToKorean()); // Prints: "일억이천삼백사십오만육천칠백팔십구"
-// }
